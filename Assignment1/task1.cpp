78cc0b222ce7cc142f9613878a24a0a663d3405b
Javaria Shahbaz <javaria222shahbaz@gmail.com>
2026-07-04 01:47:04 +0500

    Add basic user input functionality in task1.cpp

---
+++ Assignment1/task1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string name, grade;
+    int age;
+    float CGPA;
+    
+    cout << "enter name " << endl;
+    cin >> name;
+    cout << "enter age" << endl;
+    cin >> age;
+    cout << "enter grade" << endl;
+    cin >> grade;
+    cout << "enter CGPA" << endl;
+    cin >> CGPA;
+    
+    return 0;
+}
